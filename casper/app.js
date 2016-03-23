4f0acdba16650fc4f538a3343537684d7a3a3916
gkio kobyla <gkiokobyla@gkio-kobylas-MacBook.local>
2016-03-24 00:50:53 +0200

    repeat and everything done

--- casper/app.js
+++ casper/app.js
@@ -1,17 +1,25 @@
 var casper = require('casper').create();
-
+var fs = require('fs')
+var matches,refNum;
 casper.start('https://www.betbrain.com/next-matches/football', function() {
-	 	// this.click('.BoxEpilogue')
 });
+
 casper
 	.then(function(){
-		var link;
-	        link = casper.evaluate(function() {
-			return document.querySelectorAll('.TheMatch').length;
+		var name;
+	        name = casper.evaluate(function() {
+			return __utils__.findOne(".EpilogueNM .BEText").innerText;
     		});
-    console.log(link)
+    	console.log(name)
+    	//getting the number of matches only the number
+    	var regExp = /\(([^)]+)\)/;
+    	matches = regExp.exec(name)
+    	matches = matches[1].replace(/\D/g, '');
+    	// counting how much times need to refresh
+    	refNum = (matches / 50) % 1 === 0 ? matches / 50 : (matches / 50)+1
+    	refNum = /[^.]*/.exec(refNum)[0];
+    	console.log(refNum)
 	})
-	.thenClick(".EpilogueNM")
 	.then(function(){
 		var link;
 	        link = casper.evaluate(function() {
@@ -19,7 +27,25 @@ casper
     		});
     console.log(link)
 	})
-	
+	.repeat(26,function(){
+		casper.thenClick(".EpilogueNM")
+		casper.wait(1500, function() {
+			var name;
+	        name = casper.evaluate(function() {
+			return __utils__.findOne(".EpilogueNM .BEText").innerText;
+    		});
+    	console.log(name)
+    	});
+	})
+	// .then(function(){
+	// 	var name;
+	//         name = casper.evaluate(function() {
+	// 		return __utils__.findOne(".EpilogueNM .BEText").innerText;
+ //    		});
+ //    console.log(name)
+	// })
+	// .thenClick(".EpilogueNM")
+
 
 
 
