6eca6e520f34f199380b59d44a27a133b0b54783
gkio kobyla <gkiokobyla@gkio-kobylas-MacBook.local>
2016-03-23 23:08:42 +0200

    mac work

--- casper/app.js
+++ casper/app.js
@@ -3,13 +3,15 @@ var casper = require('casper').create();
 casper.start('https://www.betbrain.com/next-matches/football', function() {
 	 	// this.click('.BoxEpilogue')
 });
-casper.then(function(){
-	 	var link;
+casper
+	.then(function(){
+		var link;
 	        link = casper.evaluate(function() {
 			return document.querySelectorAll('.TheMatch').length;
     		});
     console.log(link)
-})
+	})
+
 
 
 
